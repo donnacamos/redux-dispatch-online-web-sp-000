ce11549ed66d4f5349f7e9e99f368457aaaeee64
Donna Amos <donnacamos.developer@gmail.com>
2019-12-17 22:52:33 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -7,18 +7,17 @@ function changeState(state, action){
   }
 }
 
+function render(){
+  document.body.textContent = state.count
+}
+
 function dispatch(action){
   state = changeState(state, action)
   return state
 }
 
-function render(){
-  document.body.textContent = state.count
-}
-
 render()
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+
+
